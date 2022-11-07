570c282c43f2489fb6d0e41f4ea5930f04e428de
jennyfertellez <jennyfertellez@gmail.com>
2022-11-07 13:43:42 -0800

    Added static functions to the images

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+//Set the path for express.static function
+app.use("/static", express.static("public"));
+app.use("/images", express.static("images"));
+
