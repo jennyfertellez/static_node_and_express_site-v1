e7e57f3f5a51eb93dfcc96942f3355bd0c259bf8
jennyfertellez <jennyfertellez@gmail.com>
2022-11-07 20:24:04 -0800

    set up middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
 const data = require("./data.json");
+const app = express();
+const port = 3000;
+
 
 //Set the path for express.static function
 app.use("/static", express.static("public"));
 app.use("/images", express.static("images"));
 
+//Middleware Set Up
+app.set('view engine', 'pug');
